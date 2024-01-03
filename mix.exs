cbdbf8afc630c81fe1412b3334f28090ef13fddc
madclaws <pavanan.anandu63@gmail.com>
2024-01-03 17:03:41 +0530

    fix: removed ex_ipfs_ipld lib, we ain't using that

--- mix.exs
+++ mix.exs
@@ -64,7 +64,6 @@ defmodule ExUcan.MixProject do
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
       {:b58, "~> 1.0.2"},
       {:excid, git: "https://github.com/madclaws/cid.git"},
-      {:ex_ipfs_ipld, "~> 1.0"},
       {:ex_doc, "~> 0.16", only: :dev, runtime: false}
     ]
   end
